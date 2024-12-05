ccb47792da3d70b8ad8d2fcb441c038663b99a3c
Viole Hours <holasoyviole@gmail.com>
2024-12-05 11:07:43 -0300

    assets

---
+++ client/src/assets/index.ts
@@ -0,0 +1,27 @@
+import logo from "./nav/logo.svg";
+import footerBG from "./footer/footer-bg.png";
+import logoVioleta from "./footer/Viole-Logo.webp";
+import bgBrand from "./nav/bg-hero-nav.webp";
+import york from "./brands/york.svg";
+import bgh from "./brands/bgh.svg";
+import carrier from "./brands/carrier.svg";
+import goodman from "./brands/goodman.svg";
+import ariston from "./brands/ariston.svg";
+import longvie from "./brands/longvie.svg";
+import surrey from "./brands/surrey.svg";
+import calefactor from "./product/Calefactor.png";
+
+export {
+    logo,
+    footerBG,
+    logoVioleta,
+    bgBrand,
+    york,
+    bgh,
+    carrier,
+    goodman,
+    ariston,
+    longvie,
+    surrey,
+    calefactor,
+};
